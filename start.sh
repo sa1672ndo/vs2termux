452a63fbc23d2c485c9512394cae2d62ed4c7602
sa1672ndo <87911255+sa1672ndo@users.noreply.github.com>
2024-04-10 18:04:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-
-
 JAVA_HOME=$HOME/vs2server/runtimes/jdk*
 SHELL=/data/data/com.termux/files/usr/bin/bash
 grun -s $JAVA_HOME/bin/java -Djava.library.path=$JAVA_HOME/lib -Xmx2G -jar $HOME/vs2server/jar
