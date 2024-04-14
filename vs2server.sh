30b54a38dde766f945d4d85640d9639b16153710
sa1672ndo <87911255+sa1672ndo@users.noreply.github.com>
2024-04-14 20:17:49 +0300

    merged start scripts into the installer + added misc stuff

--- vs2server.sh
+++ vs2server.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-clear
-
-SHELL=/data/data/com.termux/files/usr/bin/bash
-echo "This script should only be run in termux"
-read -n1 -r -p "Press any key to continue..."
-
 if [ "$#" -ne 3 ]; then
         echo "Usage: $0 <fabric/forge> <minecraft_version> <loader_version>"
         echo "Example: $0 fabric 1.20.4 0.15.9"
         exit 1
 fi
-
+SHELL=/data/data/com.termux/files/usr/bin/bash
+echo "This script should only be run in termux"
+read -n1 -r -p "Press any key to continue..."
+clear
 #check if mc ver is not below 1.16.5
 version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
 if version_gt "1.16.5" "$2"; then
@@ -19,6 +16,8 @@ if version_gt "1.16.5" "$2"; then
 	echo "If you want to host old minecraft server on ur phone, use any other script or set it up manually urself."
 	exit
 fi
+loader="$1"
+version="$2"
 url="https://api.modrinth.com/v2/project/valkyrien-skies/version?loaders=\[%22"$loader"%22\]&game_versions=\[%22"$version"%22\]"
 check="$(curl -s "$url" | jq '.[0]')"
 if [ -n "$check" ] && [ "$check" != "null" ]; then
@@ -32,6 +31,31 @@ iferror() {
         echo "Something went wrong, log saved to ~/.cache/vs2server.log"
         exit 1
     fi
+}
+start_script_fabric() {
+JAVA_HOME=$HOME/vs2server/runtimes/jdk*
+echo "JAVA_HOME=$HOME/vs2server/runtimes/jdk*
+SHELL=/data/data/com.termux/files/usr/bin/bash
+grun -s $JAVA_HOME/bin/java -Djava.library.path=$JAVA_HOME/lib -Xmx2G -jar $HOME/vs2server/instances/jar" > start.sh
+}
+start_script_forge(){
+JAVA_HOME=$HOME/vs2server/runtimes/jdk*
+echo "#!/usr/bin/env sh
+# Forge requires a configured set of both JVM and program argume#!/usr/bin/env sh
+# Forge requires a configured set of both JVM and program arguments.
+# Add custom JVM arguments to the user_jvm_args.txt
+# Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
+#  pass them to this script directly 
+grun -s JAVA_HOME=$HOME/vs2server/runtimes/jdk* $HOME/vs2server/runtimes/jdk*/bin/java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.2.23/unix_args.txt nogui"$@"" > start.sh
+echo "# Xmx and Xms set the maximum and minimum RAM usage, respectively.
+# They can take any number, followed by an M or a G.
+# M means Megabyte, G means Gigabyte.
+# For example, to set the maximum to 3GB: -Xmx3G
+# To set the minimum to 2.5GB: -Xms2500M
+
+# A good default for a modded server is 4GB.
+# Uncomment the next line to set it.
+-Xmx2G -Djava.library.path=$HOME/vs2server/runtimes/jdk*" > user_jvm_args.txt
 }
 
 install_temurin_jdk() {
@@ -106,7 +130,7 @@ fi
 (
 echo Installing Minecraft "$2" "$1"-"$3"
 
-if [ -d "$HOME"/vs2server ]; then
+if [ -d "$HOME"/vs2server/instances ]; then
 	echo "It seems you already ran this script before. Proceeding will delete all worlds, configs, and mods. Continue? [Y/n]"
 	read -r dc
         case "$(echo "$dc" | tr '[:upper:]' '[:lower:]')" in
@@ -140,8 +164,15 @@ echo "Installing needed termux packages"
 pkg update -y
 pkg upgrade -y
 pkg install glibc-repo -y
-pkg install glibc-runner patchelf-glibc coreutils-glibc tar coreutils patchelf openjdk-17 -y
-mkdir "$HOME"/vs2server/runtimes
+pkg install glibc-runner patchelf-glibc coreutils-glibc tar coreutils patchelf -y
+
+if [ "$1" = "forge" ]; then
+	if command -v java >/dev/null 2>&1; then
+		echo "java is already installed"
+	else
+		pkg install openjdk-17
+	fi
+fi
 
 if [ -d "$HOME"/vs2server/runtimes ]; then
 	echo "Another runtime already exists. Do you want to overwrite it with a new one?. [Y/n]"
@@ -168,29 +199,26 @@ cd "$HOME"/vs2server/instances
 
 case $1 in
 fabric )
+	grun -s JAVA_HOME=$HOME/vs2server/runtimes/jdk* $HOME/vs2server/runtimes/jdk*/bin/java -Djava.library.path=$HOME/vs2server/runtimes/jdk* -jar $HOME/vs2server/instances/jar nogui
+	start_script_fabric
 	echo "eula=true" > $HOME/vs2server/instances/eula.txt
-	grun -s JAVA_HOME=$HOME/vs2server/runtimes/jdk* $HOME/vs2server/runtimes/jdk*/bin/java -Djava.library.path=$HOME/vs2server/runtimes/jdk* -jar $HOME/vs2server/jar nogui
-	wget https://raw.githubusercontent.com/sa1672ndo/vs2termux/main/start.sh
-	sh start.sh
-        ;;
+    ;;
 forge )
-    java -jar $HOME/vs2server/instance/forge-"$2"-"$3"-installer.jar --installServer
-	rm user_jvm_args.txt
+    java -jar $HOME/vs2server/instances/forge-"$2"-"$3"-installer.jar --installServer
 	rm run.sh
-	wget https://raw.githubusercontent.com/sa1672ndo/vs2termux/main/user_jvm_args.txt
-	wget https://raw.githubusercontent.com/sa1672ndo/vs2termux/main/run.sh
+	start_script_forge
 	echo "eula=true" > $HOME/vs2server/instances/eula.txt
-	sh run.sh
     ;;
 esac
-read -p "Do you want to install Valkyrian Skies and Eureka? [Y/n]" dc
+read -p "Do you want to install Valkyrian Skies and Eureka? [Y/n] " dc
 case "$(echo "$dc" | tr '[:upper:]' '[:lower:]')" in
 [yY])
 	install_mod "$1" "$2" "eureka"
 	;;
-*)
-	exit
-	;;
 esac
+cd ~/vs2server/instances/
+sh start.sh
+pwd=$(pwd)
+echo "server files are located in "$pwd""
 
 ) 2>&1 | tee ~/.cache/vs2server.log
